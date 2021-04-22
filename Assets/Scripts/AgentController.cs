dcd5d1076f9d9a6041370a246ab0b731b1b4aa3b
j4red <jaredknof@gmail.com>
2021-04-22 00:46:31 -0700

    visualized killzone, made agents disappear after entering

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentController : MonoBehaviour {
-    public bool debugMode = false;
     private Rigidbody2D rb;
 
     [Range(1f, 10f)]
     public float speed = 5f;
-    public const float weight = 10f;
-
+    private float weight;
     private Vector3 init;
+
+    [SerializeField, Range(0.01f, 3f)]
+    private float killDelay = 2f;
+    [SerializeField, Range(0.01f, 1f)]
+    private float initDelay = 0.1f;
+
+    [SerializeField]
+    private bool debugMode = false;
+
+    public float grav = 1f;
 
     // Start is called before the first frame update
     void Start() {
         init = transform.position;
         rb = GetComponent<Rigidbody2D>();
+        weight = rb.mass;
     }
 
     // Update is called once per frame
@@ -24,7 +33,7 @@ public class AgentController : MonoBehaviour {
         float y = Input.GetAxis("Vertical");
 
         float movement = x * speed;
-        rb.velocity = new Vector2(movement, rb.velocity.y);
+        rb.velocity = new Vector2(movement, rb.velocity.y - grav);
 
 
         if (debugMode && Input.GetButtonDown("Debug Reset")) {
@@ -40,7 +49,25 @@ public class AgentController : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         /* Destroy Agent if they come into contact with a 'Kill Zone' */
         if (other.gameObject.tag == "Kill") {
-            Destroy(this.gameObject);
+            StartCoroutine("Shrink");
+            // Destroy(this.gameObject, killDelay);
         }
     }
+
+    IEnumerator Shrink() {
+        /* Coroutine code based on Unity manual, https://docs.unity3d.com/Manual/Coroutines.html. */
+        // float initDelay = 1f;
+
+        for (float i = 0; i < initDelay; i += Time.deltaTime) {
+            /* This code simply waits initDelay seconds before triggering the shrink effect. */
+            yield return null;
+        }
+        
+        for (float i = 0; i <= killDelay; i += Time.deltaTime) {
+            transform.localScale = transform.localScale * .95f; // shrink agent by 5% each frame
+            yield return null;
+        }
+        Destroy(this.gameObject);
+    }
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public bool debugMode = false;
-
+    [Range(1, 120)]
+    public int frameDelay = 12; // num frames to wait between each instance of new agent
+    private int delay = 0;
+    [SerializeField, Range(0, 1f)]
+    private float randomOffset = 0.1f;
     public GameObject agent;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (debugMode && Input.GetMouseButton(0)) {
-            /* If debug mode is enabled and left mouse button pressed: */
-            Instantiate(agent, new Vector3(0, 0, 0), Quaternion.identity);
+            /* If debug mode is enabled and left mouse button pressed, create new agents at mouse position: */
+            delay -= 1;
+            if (delay <= 0) {
+                Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                newPos.x += Random.Range(-randomOffset, randomOffset);
+                newPos.y += Random.Range(-randomOffset, randomOffset);
+                newPos.z = 0f;
+                Instantiate(agent, newPos, Quaternion.identity);
+                delay = frameDelay;
+            }
         }
     }
 }
