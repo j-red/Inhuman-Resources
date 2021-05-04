2a936f5b65ae93b86b4b8e5acbec6089533ef092
A. McNamee <12386516+audmcname@users.noreply.github.com>
2021-05-04 14:28:12 -0700

    Update DialogueManager.cs

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -14,7 +14,7 @@ public class DialogueManager : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         sentences = new Queue<string>();
   
