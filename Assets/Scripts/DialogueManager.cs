b795485ff48ef1a22887f3b1be01b4cd6398adb0
j4red <jaredknof@gmail.com>
2021-05-04 15:45:06 -0700

    tried adding color to dialogue system

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,55 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogueManager : MonoBehaviour
-{
+public class DialogueManager : MonoBehaviour {
     public GameObject dialogueBox;
     public Text dialogueText;
-
     public bool dialogueActive;
-
     public Queue<string> sentences;
+    private GameManager gm;
+    private AudioSource audioSrc;
+    public float delayTime = 0.005f;
 
+    [HeaderAttribute ("Audio")]
+    // public AudioClip soundEffect; // not used; see the clip for the audio source instead
+    [Range(0, 1f), Tooltip("Noise factor for pitch modulation.")]
+    public float pitchModulation = 0.1f;
 
     // Start is called before the first frame update
-    void Awake()
-    {
+    void Awake() {
         sentences = new Queue<string>();
-  
+        gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        audioSrc = GetComponent<AudioSource>();
     }
-    // Congratulations, human, on being randomly selected for this worthwhile and highly enjoyable job.
-    // Look, your fellow human workers aren't going to their next job. They need instructions.
-    // Move quickly. You don't want to disappoint me. 
-
 
     // Update is called once per frame
-    void Update()
-    {
-        if (dialogueActive && Input.GetButtonUp("Primary"))
-        {
+    void Update() {
+        if (dialogueActive && Input.GetButtonDown("Primary") && !gm.isPaused) {
             DisplayNextSentence();
         }
     }
 
-    public void StartDialogue (Dialogue dialogue)
-    {
-
+    public void StartDialogue (Dialogue dialogue) {
         sentences.Clear();
         dialogueActive = true;
         dialogueBox.SetActive(true);
 
-        foreach (string sentence in dialogue.sentences)
-        {
-        
+        foreach (string sentence in dialogue.sentences) {
             sentences.Enqueue(sentence);
         }
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence()
-    {
-        if (sentences.Count == 0)
-        {
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
             EndDialogue();
             return;
         }
@@ -60,22 +52,25 @@ public class DialogueManager : MonoBehaviour
         StartCoroutine(TypeSentence(sentence));
     }
 
-    IEnumerator TypeSentence (string sentence) // animates dialogue one letter at a time
-    {
+    IEnumerator TypeSentence (string sentence) { // animates dialogue one letter at a time 
         dialogueText.text = "";
-        foreach (char letter in sentence.ToCharArray())
-        {
+        float tmpPitch = audioSrc.pitch;
+        foreach (char letter in sentence.ToCharArray()) {
             dialogueText.text += letter;
-            yield return null;
+
+            audioSrc.pitch = tmpPitch; // reset to original
+            if (audioSrc.clip != null && !audioSrc.isPlaying) {
+                audioSrc.pitch = Random.Range(tmpPitch - pitchModulation, tmpPitch + pitchModulation); // add some noise
+                print(audioSrc.pitch);
+                audioSrc.Play();
+            }
+            yield return new WaitForSeconds(delayTime);
         }
     }
 
-    void EndDialogue()
-    {
+    void EndDialogue() {
         dialogueBox.SetActive(false);
         dialogueActive = false;
     }
-
-
 
 }
